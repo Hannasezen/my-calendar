65896b55e0f5be958cd6103e59ff778f700f8a32
Hannasezen <andreeva0649@gmail.com>
2018-11-14 20:20:19 +0200

    close button done

--- src/CalendarDay.vue
+++ src/CalendarDay.vue
@@ -23,6 +23,7 @@ export default {
     methods: {
         captureClick(event) {
             this.$store.commit('eventFormPos', { x: event.clientX, y: event.clientY });
+            this.$store.commit('eventFormActive', true);
         }
     }
 }
--- src/EventForm.vue
+++ src/EventForm.vue
@@ -1,18 +1,29 @@
 <template>
-    <div id="event-form" :class="{ active: true }" :style="{ top: top, left: left }"></div>
+    <div id="event-form" :class="{ active: active }" :style="{ top: top, left: left }">
+        <h4>Add an event</h4>
+        <button id="close-button" @click="close">x</button>
+    </div>
 </template>
 
 <script>
-export default {
-    computed: {
-        top() {
-            return `${this.$store.state.eventFormPosY}px`;
+    export default {
+        methods: {
+            close() {
+                this.$store.commit('eventFormActive', false);
+            }
         },
-        left() {
-            return `${this.$store.state.eventFormPosX}px`;
+        computed: {
+            active() {
+                return this.$store.state.eventFormActive;
+            },
+            top() {
+                return `${this.$store.state.eventFormPosY}px`;
+            },
+            left() {
+                return `${this.$store.state.eventFormPosX}px`;
+            }
         }
     }
-}
 </script>
 
 <style>
