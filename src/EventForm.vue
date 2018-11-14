df096a1b71dc6c6dfd66ec4c949093f35fbf6cfa
Hannasezen <andreeva0649@gmail.com>
2018-11-14 23:00:38 +0200

    edit create method

--- src/EventForm.vue
+++ src/EventForm.vue
@@ -2,7 +2,7 @@
     <div id="event-form" :class="{ active: active }" :style="{ top: top, left: left }">
         <h4>Add an event</h4>
         <div class="text">
-            <input type="text" v-model="description">
+            <input type="text" v-model="description" placeholder="My next plan is...">
         </div>
         <div class="buttons">
             <button @click="create">Create</button>
@@ -23,8 +23,11 @@
                 this.$store.commit('eventFormActive', false);
             },
             create() {
-                this.$store.commit('addEvent', this.description);
-                this.close();
+                if(this.description.length > 0) {
+                    this.$store.commit('addEvent', this.description);
+                    this.description = '';
+                    this.close();
+                }               
             }
         },
         computed: {
